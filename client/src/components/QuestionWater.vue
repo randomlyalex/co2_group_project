24f8e1535e4ba3c4ae0b97c5546674f5dfec5514
Lyolya <almasilaura@outlook.com>
2021-01-23 21:45:19 +0000

    ADDED vue directives to Wateer template

--- client/src/components/QuestionWater.vue
+++ client/src/components/QuestionWater.vue
@@ -5,15 +5,14 @@
             <p>How much water used in your household?</p>
                 <form>
                     <label for="water-amount1">Enter amount:</label>
-                    <input type="number" id="water-amount1" name="quantity"><br>
+                    <input type="number" id="water-amount1" name="quantity" v-model="waterAmount"><br>
                     <input type="submit" value="Submit">
                 </form> 
-              
-    
   </div>
 </template>
 
 <script>
+
 
 export default {
 name: "question-water",
