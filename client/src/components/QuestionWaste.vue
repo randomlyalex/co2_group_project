16b3e2792f432cc6f1e610bffa5bbcc7e3870910
Lyolya <almasilaura@outlook.com>
2021-01-23 21:58:07 +0000

    ADDED vue directives to Waste template

--- client/src/components/QuestionWaste.vue
+++ client/src/components/QuestionWaste.vue
@@ -5,15 +5,14 @@
             <p>How much waste is produced in your household in a week?</p>
                 <form>
                     <label for="waste-amount1">Enter amount:</label>
-                    <input type="number" id="waste-amount1" name="quantity"><br>
+                    <input type="number" id="waste-amount1" name="quantity" v-model="wasteAmount"><br>
                     <input type="submit" value="Submit">
                 </form> 
-                
-
   </div>
 </template>
 
 <script>
+
 
 export default {
 name: "question-waste",
