923b9fd2b6f2e94323da6f494deef962a16962e8
Alex Dodd <alexdodd@gmail.com>
2021-01-26 22:36:53 +0000

    this code has been moved to the results section

--- client/src/components/StuffSection.vue
+++ client/src/components/StuffSection.vue
@@ -1,46 +1,37 @@
 <template>
-    <div>
-        <question-component
-            v-for="(question, index) in questions"
-            :key="index"
-            v-bind:data="question"
-            v-on:question_co2_total="questions[index].co2total = $event"
-            v-on:question_answers="questions[index].answers = $event"
-        >
-        </question-component>
-        <button v-on:click="saveSection">SaveSection</button>
-    </div>
+	<div>
+		<question-component
+			v-for="(question, index) in questions"
+			:key="index"
+			v-bind:data="question"
+			v-on:question_co2_total="questions[index].co2total = $event"
+			v-on:question_answers="questions[index].answers = $event"
+		>
+		</question-component>
+		<button v-on:click="saveSection">SaveSection</button>
+	</div>
 </template>
 
 <script>
-import QuestionComponent from "./QuestionComponent.vue";
-import StuffQuestions from "../static/stuff_questions.json";
+import QuestionComponent from './QuestionComponent.vue';
+import StuffQuestions from '../static/stuff_questions.json';
 
 export default {
-    name: "StuffSection",
-    components: {
-        "question-component": QuestionComponent,
-    },
-    methods: {
-        saveSection: function() {
-            this.$emit("stuff_questions", this.questions);
-        },
-    },
+	name: 'StuffSection',
+	components: {
+		'question-component': QuestionComponent,
+	},
+	methods: {
+		saveSection: function() {
+			this.$emit('stuff_questions', this.questions);
+		},
+	},
 
-    data() {
-        return {
-            questions: StuffQuestions,
-        };
-    },
-    computed: {
-        total_stuff_co2: function() {
-            return this.questions.reduce((total, question) => {
-                if (question.co2total >= 0) {
-                    return total + question.co2total;
-                }
-            }, 0);
-        },
-    },
+	data() {
+		return {
+			questions: StuffQuestions,
+		};
+	},
 };
 </script>
 
