bb0118abc1f3bd11f765c3fe8f114dc14a1c0cbe
Alex Dodd <alexdodd@gmail.com>
2021-01-26 15:04:12 +0000

    implemented the chart and
    populated with dynamic results

--- client/src/components/ResultSection.vue
+++ client/src/components/ResultSection.vue
@@ -1,10 +1,33 @@
 <template lang="html">
-	<div>Your carbon footprint is {{ diet_questions[0].co2total }}</div>
+	<div>
+		<div>Your carbon footprint is {{ diet_questions[0].co2total }}</div>
+		<apexchart
+			width="500"
+			type="bar"
+			v-bind:options="this.chartOptions"
+			v-bind:series="this.series"
+		></apexchart>
+	</div>
 </template>
-
 <script>
+import VueApexCharts from 'vue-apexcharts';
 export default {
 	name: 'ResultSection',
+	components: {
+		apexchart: VueApexCharts,
+	},
+	data: function() {
+		return {
+			chartOptions: {
+				chart: {
+					id: 'vuechart-example',
+				},
+				xaxis: {
+					categories: ['Diet', 'Stuff', 'Transport', 'Home'],
+				},
+			},
+		};
+	},
 	props: [
 		'diet_questions',
 		'transport_questions',
@@ -12,6 +35,19 @@ export default {
 		'stuff_questions',
 	],
 	computed: {
+		series: function() {
+			return [
+				{
+					name: 'series1',
+					data: [
+						this.total_diet_co2,
+						this.total_stuff_co2,
+						this.total_transport_co2,
+						14,
+					],
+				},
+			];
+		},
 		total_diet_co2: function() {
 			return this.diet_questions.reduce((total, question) => {
 				if (question.co2total) {
