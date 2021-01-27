99993a71cbecc648aab0312a0d4a930f19b0d94a
Alex Dodd <alexdodd@gmail.com>
2021-01-27 22:46:06 +0000

    moved results to the side (badly)

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,66 +1,82 @@
 <template>
 	<div id="app">
-		<button v-on:click="sectionCounter = 7">Admin</button>
-		<landing-page v-if="sectionCounter === 0"> </landing-page>
-		<button v-if="sectionCounter === 0" v-on:click="sectionCounter = 1">
-			Start
-		</button>
-		<div v-if="sectionCounter !== 0">
-			<button v-on:click="sectionCounter = 1">Diet</button>
-			<button v-on:click="sectionCounter = 2">Transport</button>
-			<button v-on:click="sectionCounter = 3">Stuff</button>
-			<button v-on:click="sectionCounter = 4">Home</button>
-			<button v-on:click="sectionCounter = 5">Results</button>
+		<div class="top-container-menu">
+			<button v-on:click="sectionCounter = 7">Admin</button>
+			<landing-page v-if="sectionCounter === 0"> </landing-page>
+			<button v-if="sectionCounter === 0" v-on:click="sectionCounter = 1">
+				Start
+			</button>
+			<div v-if="sectionCounter !== 0">
+				<button v-on:click="sectionCounter = 1">Diet</button>
+				<button v-on:click="sectionCounter = 2">Transport</button>
+				<button v-on:click="sectionCounter = 3">Stuff</button>
+				<button v-on:click="sectionCounter = 4">Home</button>
+				<button
+					v-on:click="
+						{
+							sectionCounter = 5;
+							resultsVisted = true;
+						}
+					"
+				>
+					Results
+				</button>
+			</div>
 		</div>
-
-		<br />
-		<diet-section
-			v-if="sectionCounter === 1"
-			v-on:diet_questions="
-				{
-					diet_questions = $event;
-					sectionCounter += 1;
-				}
-			"
-		></diet-section>
-		<transport-section
-			v-if="sectionCounter === 2"
-			v-on:transport_questions="
-				{
-					transport_questions = $event;
-					sectionCounter += 1;
-				}
-			"
-		></transport-section>
-		<stuff-section
-			v-if="sectionCounter === 3"
-			v-on:stuff_questions="
-				{
-					stuff_questions = $event;
-					sectionCounter += 1;
-				}
-			"
-		></stuff-section>
-		<home-section
-			v-if="sectionCounter === 4"
-			v-on:home_questions="
-				{
-					home_questions = $event;
-					sectionCounter += 1;
-				}
-			"
-		></home-section>
-		<result-section
-			v-if="sectionCounter === 5"
-			v-bind:diet_questions="diet_questions"
-			v-bind:transport_questions="transport_questions"
-			v-bind:stuff_questions="stuff_questions"
-			v-bind:home_questions="home_questions"
-		></result-section>
-		<admin-section v-if="sectionCounter === 7"></admin-section>
-		<button v-if="sectionCounter === 5" v-on:click="sectionCounter = 0">
+		<div class="main-container">
+			<div class="left-container">
+				<diet-section
+					v-if="sectionCounter === 1"
+					v-on:diet_questions="
+						{
+							diet_questions = $event;
+							sectionCounter += 1;
+						}
+					"
+				></diet-section>
+				<transport-section
+					v-if="sectionCounter === 2"
+					v-on:transport_questions="
+						{
+							transport_questions = $event;
+							sectionCounter += 1;
+						}
+					"
+				></transport-section>
+				<stuff-section
+					v-if="sectionCounter === 3"
+					v-on:stuff_questions="
+						{
+							stuff_questions = $event;
+							sectionCounter += 1;
+						}
+					"
+				></stuff-section>
+				<home-section
+					v-if="sectionCounter === 4"
+					v-on:home_questions="
+						{
+							home_questions = $event;
+							sectionCounter += 1;
+							resultsVisted = true;
+						}
+					"
+				></home-section>
+				<admin-section v-if="sectionCounter === 7"></admin-section>
+			</div>
+			<div class="right-container">
+				<result-section
+					v-if="sectionCounter === 5 || resultsVisted === true"
+					v-bind:diet_questions="diet_questions"
+					v-bind:transport_questions="transport_questions"
+					v-bind:stuff_questions="stuff_questions"
+					v-bind:home_questions="home_questions"
+				></result-section>
+			</div>
+		</div>
+		<!-- <button v-if="sectionCounter === 5" v-on:click="sectionCounter = 0">
 			Back to Start
-		</button>
+		</button> -->
 	</div>
 </template>
 
@@ -83,6 +99,7 @@ export default {
 			home_questions: [],
 			selectedSection: 'diet',
 			sectionCounter: 0,
+			resultsVisted: false,
 		};
 	},
 	components: {
@@ -105,5 +122,8 @@ export default {
 	text-align: center;
 	color: #2c3e50;
 	margin-top: 60px;
+}
+.main-container {
+	display: flex;
 }
 </style>
