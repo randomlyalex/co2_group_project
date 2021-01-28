0901c4727ef97799dd8d46b2a629a06a91977504
Lyolya <almasilaura@outlook.com>
2021-01-28 12:07:15 +0000

    EDITED the order of labels for graph

--- client/src/components/ResultSection.vue
+++ client/src/components/ResultSection.vue
@@ -25,7 +25,7 @@ export default {
 	data: function() {
 		return {
 			chartOptions: {
-				labels: ['Diet', 'Transport', 'Home', 'Stuff'],
+				labels: ['Diet', 'Transport', 'Stuff', 'Home'],
 			},
 		};
 	},
